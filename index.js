a60e6dbc7964bc84b2e2dc40331de7c9a5f2f12a
Ville Lyytikäinen <corpsefin@gmail.com>
2022-02-01 10:01:52 +0200

    adding to heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const express = require('express');
 const persons = require('./persons.json')
+const cors = require('cors')
 const morgan = require('morgan')
 const app = express();
 const PORT = 3001;
@@ -12,6 +13,7 @@ const generateId = () => {
 
 morgan.token('postBody', (req) => JSON.stringify(req.body))
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :postBody"))
 
@@ -75,7 +77,7 @@ app.delete('/api/persons/:id', (req,res) => {
     res.status(204).end()
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log(`Server running at port ${PORT}`)
 })
 
